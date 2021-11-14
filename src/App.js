086583b53eccaa33403a4349b02574007dbb6fe8
Manasi <manasiyadav2508@gmail.com>
2021-11-15 01:28:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css";
+import About from './pages/About';
+import Home from './pages/Home';
+import AddEdit from './pages/AddEdit';
+import view from './pages/view';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <div className ="App">
+     <header />
+     <ToastContainer/>
+     <Switch>
+       <Route exact path="/" component={Home} />
+       <Route exact path="/add" component={AddEdit} />
+       <Route exact path="/update" component={AddEdit}/>
+       <Route exact path="/about" component={About} />
+       <Route exact path="/view/:id" component={view} />
+     </Switch>
+   </div>
+   </BrowserRouter>
   );
 }
 
